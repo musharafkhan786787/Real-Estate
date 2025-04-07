2b376413eb5d556bcb7d7f668f9666ab77e4c58c
Musharaf <your-email@example.com>
2025-04-06 23:12:18 -0700

    initial

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\my pc\pycharmprojects\sideproject\realestate\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
